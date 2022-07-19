9f27797846dc5b0b688c41871277c408a2b17e7d
tuanhuy030799@gmmail.com <huy.nguyentuan@tpptechnology.com>
2022-07-19 14:36:09 +0700

    blueprint blog

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask
 from . import db
-from . import auth
+from . import auth, blog
 
 def create_app(test_config=None):
     # create and configure the app
@@ -36,5 +36,8 @@ def create_app(test_config=None):
 
    
     app.register_blueprint(auth.bp)
+
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
